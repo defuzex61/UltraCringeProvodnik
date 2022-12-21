fd4d265a241aa17b816b9bb9d185399de47535b7
defuzex61 <isip_r.r.korniyk@mpt.ru>
2022-12-21 23:11:04 +0300

    first commit

---
+++ CringeProvodnik/MainMenu.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CringeProvodnik
+{
+    internal class MainMenu
+    {
+        public static void FileMenu(string DirPath)
+        {
+            int CurrentLine = 0;
+            while (true)
+            {
+
+
+                string[] allDirectories = Directory.GetDirectories(DirPath);
+                string[] allFiles = Directory.GetFiles(DirPath);
+                Console.WriteLine("Навигация - стрелки / вход - enter  / выход - escape ");
+                Console.WriteLine(DirPath);
+                Console.WriteLine("");
+
+                foreach (string Dir in allDirectories)
+                {
+                    if (CurrentLine == Array.IndexOf(allDirectories, Dir))
+                    {
+                        Console.WriteLine($"--> {Dir}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"     {Dir}");
+                    }
+                }
+
+                foreach (string file in allFiles)
+                {
+                    if (CurrentLine == (Array.IndexOf(allFiles, file) + allDirectories.Length))
+                    {
+                        Console.WriteLine($"--> {file}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"     {file}");
+                    }
+                }
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow & CurrentLine > 0)
+                {
+                    CurrentLine--;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.DownArrow & CurrentLine < (allDirectories.Length + allFiles.Length - 1))
+                {
+                    CurrentLine++;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+
+                    if (CurrentLine <= allDirectories.Length)
+                    {
+                        MainMenu.FileMenu(allDirectories[CurrentLine]);
+                       
+                    }
+
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    Console.Clear();
+                    break;
+
+                }
+
+               
+
+                else
+                {
+                    Console.Clear();
+                }
+              
+            }
+
+
+        }
+
+       
+        
+    }
+    
+}
---
+++ CringeProvodnik/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace CringeProvodnik
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int CurrentLine = 1;
+           
+            
+            while (true)
+            {
+                Console.WriteLine("Программа показывает только папки(");
+                int DiskNum = 1;
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (DriveInfo drive in drives)
+                {
+                    if (DiskNum == CurrentLine)
+                    {
+                        Console.WriteLine(
+                            $"--> {DiskNum}. Диск {drive.Name} Свободно {drive.AvailableFreeSpace / 1073741824} " +
+                            $"Гб из {drive.TotalSize / 1073741824} Гб");
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"    {DiskNum}. Диск {drive.Name} Свободно {drive.AvailableFreeSpace / 1073741824} " +
+                            $"Гб из {drive.TotalSize / 1073741824} Гб");
+                    }
+                    DiskNum++;
+                }
+               
+              
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow )
+                {
+                    CurrentLine--;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.DownArrow )
+                {
+                    CurrentLine++;
+                    Console.Clear();
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    MainMenu.FileMenu(drives[CurrentLine - 1].Name);
+
+                }
+                else
+                {
+                    Console.Clear();
+                }
+
+            }
+        }
+        
+    }
+}
